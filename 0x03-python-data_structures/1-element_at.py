340b7340241cc101bfbf12e6427a95ed76b356bf
chriss1525 <okothchris15@gmail.com>
2023-01-11 09:52:08 +0300

    retrieve element from list/fix if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-        if idx > (len(my_list) - 1) and idx < 0:
-            return (None)
-        else:
-            return (my_list[idx])
+    if idx > (len(my_list) - 1):
+        return (None)
+    elif idx < 0:
+        return (None)
+    else:
+        return (my_list[idx])
