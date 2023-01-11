df2ca19f9ebacee6b0f8a15be83cf55709c3f9c1
chriss1525 <okothchris15@gmail.com>
2023-01-11 10:07:47 +0300

    return element in list

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
+replace_in_list = __import__('2-replace_in_list').replace_in_list
 
-list = [1, 2, 3]
-idx = -1
-print("Element at index {:d} is {}".format(idx, element_at(list, idx)))
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
