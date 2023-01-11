689aad1806f36bdbe7a5591ad404fc996340041f
chriss1525 <okothchris15@gmail.com>
2023-01-11 09:45:20 +0300

    ritrieve and element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+        if idx > (len(my_list) - 1) and idx < 0:
+            return (None)
+        else:
+            return (my_list[idx])
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
+element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
