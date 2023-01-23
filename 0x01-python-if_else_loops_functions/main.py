b92b6323efbebb8a9604a37b65d5c56820d8f368
chriss1525 <okothchris15@gmail.com>
2023-01-23 21:12:15 +0300

    fizzbuzz but in python!

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-pow = __import__('11-pow').pow
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+fizzbuzz()
+print("")
