fc5febd7415e76780d0bbddfd17c7f895f42a68e
chriss1525 <okothchris15@gmail.com>
2023-01-23 21:40:18 +0300

    create a copy of a string removing item from nth position

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+remove_char_at = __import__('101-remove_char_at').remove_char_at
 
-fizzbuzz()
-print("")
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
