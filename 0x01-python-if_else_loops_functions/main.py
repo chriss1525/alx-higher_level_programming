86b842a2b4d8d8a9bdc01473508db6a42a7e540d
chriss1525 <okothchris15@gmail.com>
2023-01-23 21:02:11 +0300

    compute a to the power of b

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-add = __import__('10-add').add
+pow = __import__('11-pow').pow
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
