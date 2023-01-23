5ee63f04a49f1619341524335a583a76b3619f2c
chriss1525 <okothchris15@gmail.com>
2023-01-23 20:57:05 +0300

    add two numbers

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+add = __import__('10-add').add
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
