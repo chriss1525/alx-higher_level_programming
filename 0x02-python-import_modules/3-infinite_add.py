17bfe72cf18fc158c2f8394335bb03704e7aeda0
chriss1525 <okothchris15@gmail.com>
2023-03-09 17:49:23 +0300

    add all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     argc = len(argv)
     t_args = 0
     for index in range(argc - 1):
-       t_args += int(argv[index + 1])
-    print("{}".format(t_args))
+        t_args += int(argv[index + 1])
+        print("{}".format(t_args))
