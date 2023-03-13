60af2f8698dfeb8467d834a312779e7f0da0f6f3
chriss1525 <okothchris15@gmail.com>
2023-03-13 09:11:12 +0300

    ad item at index

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
+#!/usr/bin/python3
+new_in_list = __import__('4-new_in_list').new_in_list
 
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
