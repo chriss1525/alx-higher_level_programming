1492b5f9cb9d4b33e8af14a04d735fd29b7209f1
chriss1525 <okothchris15@gmail.com>
2023-03-13 09:31:49 +0300

    remove item from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newstring = " "
+    newstring = ""
     for i in range(len(my_string)):
         if not (my_string[i] == 'c' or my_string[i] == 'C'):
             newstring += my_string[i]
