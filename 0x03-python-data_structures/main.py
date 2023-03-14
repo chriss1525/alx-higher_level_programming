9f3855b9d0eb7796b85188e0af64281c1b9bb879
chriss1525 <okothchris15@gmail.com>
2023-03-15 01:56:21 +0300

    delete item at index

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+delete_at = __import__('11-delete_at').delete_at
 
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(
-        my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
