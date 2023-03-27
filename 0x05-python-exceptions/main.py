2cca754d51ac415791f2d0322dcebc0f3a97024d
chriss1525 <okothchris15@gmail.com>
2023-03-28 00:27:41 +0300

    raise exception with message

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-raise_exception = __import__('5-raise_exception').raise_exception
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
 
 try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
