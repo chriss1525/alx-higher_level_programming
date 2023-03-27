81f6d7b68bb82e02f9cf46998bf666af1702e2ab
chriss1525 <okothchris15@gmail.com>
2023-03-27 21:16:55 +0300

    remove bare except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             len += 1
-        except:
+        except Exception:
             break
     print("")
     return len
