1a54f9cc697f8aebbde13c6fd4adec6238356800
chriss1525 <okothchris15@gmail.com>
2023-03-27 21:14:03 +0300

    print x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    len = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            len += 1
+        except:
+            break
+    print("")
+    return len
