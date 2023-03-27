8a540825ef8531e1957effa8b52de9ded3535ea0
chriss1525 <okothchris15@gmail.com>
2023-03-28 00:43:11 +0300

    print integer

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+safe_print_integer_err = \
+    __import__('100-safe_print_integer_err').safe_print_integer_err
 
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
