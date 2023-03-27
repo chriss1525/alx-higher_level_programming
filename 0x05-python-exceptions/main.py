b4228912021eced7a6f7bccd4769039aaa7d8134
chriss1525 <okothchris15@gmail.com>
2023-03-27 21:25:02 +0300

    print integer

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-my_list = [1, 2, 3, 4, 5]
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
