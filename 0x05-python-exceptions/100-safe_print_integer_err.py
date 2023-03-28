46e95910cab4eb6b97eac60045b4c3ad5710695d
chriss1525 <okothchris15@gmail.com>
2023-03-28 11:35:34 +0300

    print an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception:
-        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr))
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-safe_print_integer_err = \
-    __import__('100-safe_print_integer_err').safe_print_integer_err
+safe_function = __import__('101-safe_function').safe_function
 
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
 
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+def my_div(a, b):
+    return a / b
 
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
+
+
+def print_list(my_list, len):
+    i = 0
+    while i < len:
+        print(my_list[i])
+        i += 1
+    return len
+
+
+result = safe_function(print_list, [1, 2, 3, 4], 10)
+print("result of print_list: {}".format(result))
