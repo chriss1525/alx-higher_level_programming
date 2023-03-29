e6062f0372beac576a157f7b2f3e8f780e049e13
chriss1525 <okothchris15@gmail.com>
2023-03-29 18:07:24 +0300

    define a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""class square"""
+"""
+define class square
+if size is not an integer raise type error
+if size is less than 0 raise value error
+"""
 
 
 class Square:
-    """define object size"""
+    """
+    define object size
+    size is an int that is greater than or equal to 0
+    """
 
     def __init__(self, size=0):
         """initialize size"""
         if type(size) is int:
-            if size >= 0:
-                self.__size = size
-                """handle errors"""
-            else:
-                raise ValueError("size must be >=0")
+            pass
         else:
             raise TypeError("size must be an integer")
+        if size >= 0:
+            self.__size = size
+        else:
+            raise ValueError("size must be >= 0")
