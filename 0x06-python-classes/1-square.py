933b3096d7a11b0349596a89b365ce6146569aa9
chriss1525 <okothchris15@gmail.com>
2023-03-29 17:12:31 +0300

    create an object/instance inside a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a class with an object size in it"""
+
+
+class Square:
+    """defining a private object size inside class square"""
+    def __init__(self, size):
+        """initializing object"""
+        self.__size = size
