a12692ee6335d50d77a919024afc9488cfc6c0fe
chriss1525 <okothchris15@gmail.com>
2023-03-30 05:44:14 +0300

    create class node and class singly linked list

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-Square = __import__('6-square').Square
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
