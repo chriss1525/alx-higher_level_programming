a328c59eea6205f0035bb101bcef999b97a09fd5
chriss1525 <okothchris15@gmail.com>
2023-04-04 15:13:11 +0300

    creating test cases for a function that adds to integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    Returns the sum of a and b
+
+    >>>add_integer(3, 5)
+    8
+    >>>add_integer(1)
+    99
+    """
     if type(a) == float:
+        """cast floats to integers"""
         a = int(a)
     if type(b) == float:
         b = int(b)
     elif type(a) != int:
+        """raise typeerror when number is not a float or integer"""
         raise TypeError("a must be an integer")
     elif type(b) != int:
         raise TypeError("b must be an integer")
--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -13,3 +13,7 @@ try:
     print(add_integer(None))
 except Exception as e:
     print(e)
+try:
+    print(add_integer(8, 'j'))
+except Exception as e:
+    print(e)
