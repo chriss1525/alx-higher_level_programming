797b11e02bb6bb6d6fb1923dcecb18bae17aea5f
chriss1525 <okothchris15@gmail.com>
2023-04-07 11:58:32 +0300

    replace item in list with new item

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    rlist = []
+    for i in my_list:
+        if i == search:
+            rlist.append(replace)
+        else:
+            rlist.append(i)
+    return rlist
