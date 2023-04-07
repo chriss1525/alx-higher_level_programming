51ee658c8f77214fc2ab359e0f082c46c559cbd5
chriss1525 <okothchris15@gmail.com>
2023-04-07 12:06:27 +0300

    add all unique numbers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq = []
+    sum = 0
+    for i in my_list:
+        if i not in uniq:
+            sum += i
+            uniq.append(i)
+    return sum
