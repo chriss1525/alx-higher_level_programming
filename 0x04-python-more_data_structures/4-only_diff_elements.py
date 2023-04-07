ba3e13fbb9396315fa555c87b2d8f33ba888169e
chriss1525 <okothchris15@gmail.com>
2023-04-07 13:36:49 +0300

    list of elements only in one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    seplist = []
+    for i in set_1:
+        if i not in set_2:
+            seplist.append(i)
+
+    for i in set_2:
+        if i not in set_1:
+            seplist.append(i)
+    return seplist
