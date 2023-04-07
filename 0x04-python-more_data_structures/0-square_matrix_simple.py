3f2bbeeb91930779199aba15988ad7dac3f74d28
chriss1525 <okothchris15@gmail.com>
2023-04-07 11:48:39 +0300

    squares all integers in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
+    nmatrix = []
+    for i in range(len(matrix)):
+        nmatrix.append([])
+
+        for j in range(len(matrix[i])):
+            nmatrix[i].append(matrix[i][j] ** 2)
+
+    return nmatrix
