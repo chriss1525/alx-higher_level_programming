46be0973778469e8ab173e57ed939a2bf5e8877e
chriss1525 <okothchris15@gmail.com>
2023-04-14 13:15:55 +0300

    print a square using # symbol

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module defines print_square"""
+def print_square(size):
+    """prints a square of side size using the # symbol"""
+    if not isinstance(size, int):
+        """check if size is an integer"""
+        raise TypeError("size must be an integer")
+    if size < 0:
+        """check if size is greater than 0"""
+        raise ValueError("size must be >= 0")
+    print((("#" * size) + "\n") * size, end="")
