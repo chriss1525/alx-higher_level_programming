a6362663c8f2860a390306fc54a7b711b2bce915
chriss1525 <okothchris15@gmail.com>
2023-04-17 21:51:02 +0300

    create Base module + tests for base module

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This test module defines tests for base.py"""
+
+from models.base import Base
+import unittest
+
+
+class TestBase(unittest.TestCase):
+    def test_init_with_id(self):
+        # Test that the id attribute is set correctly when provided
+        obj = Base(42)
+        self.assertEqual(obj.id, 42)
+
+    def test_init_without_id(self):
+        # Test that the id attribute is incremented correctly when not provided
+        obj1 = Base()
+        obj2 = Base()
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
+
+
+# if __name__ == '__main__':
+    unittest.main()
