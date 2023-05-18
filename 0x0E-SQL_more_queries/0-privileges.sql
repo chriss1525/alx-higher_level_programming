e38d58e710304a601c37a1c0de82bc160ca9b68a
chriss1525 <okothchris15@gmail.com>
2023-05-18 13:59:11 +0300

    show user priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- show user privilenges for two users
--- show privileges for user_od_1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privileges for user_od_2
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_1 @localhost;
+SHOW GRANTS FOR user_0d_2 @localhost;
