a946dc3554f780ff437491e38fae77eba131beb0
chriss1525 <okothchris15@gmail.com>
2023-05-18 14:08:38 +0300

    createa  user and grant them all privilleges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,4 @@
 -- create new user
 CREATE USER IF NOT EXISTS user_0d_1 @localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- grant user all privileges
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1 @localhost WITH
-GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1 @localhost;
