e623b5cee1e6872fcee0c5060ccbd1bd32d59030
chriss1525 <okothchris15@gmail.com>
2023-05-18 13:50:25 +0300

    add comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- show user privilenges
+-- show user privilenges for two users
+-- show privileges for user_od_1
 SHOW GRANTS FOR 'user_0d_1' @'localhost';
+-- show privileges for user_od_2
 SHOW GRANTS FOR 'user_0d_1' @'localhost';
