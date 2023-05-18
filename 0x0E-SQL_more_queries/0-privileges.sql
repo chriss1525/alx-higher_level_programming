3280880645c24299ea76f687506f91e77e51961d
chriss1525 <okothchris15@gmail.com>
2023-05-18 13:52:29 +0300

    remove quotation

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- show user privilenges for two users
 -- show privileges for user_od_1
-SHOW GRANTS FOR 'user_0d_1' @'localhost';
+SHOW GRANTS FOR user_0d_1 @localhost;
 -- show privileges for user_od_2
-SHOW GRANTS FOR 'user_0d_1' @'localhost';
+SHOW GRANTS FOR user_0d_1 @localhost;
