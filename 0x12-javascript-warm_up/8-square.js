794c478a4089cda1a07ebd13c06ab4630dfe81f2
chriss1525 <okothchris15@gmail.com>
2023-06-13 00:51:50 +0300

    print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const number = parseInt(args[2], 10);
+let i;
+let j;
+
+if (args[2]) {
+  if (number) {
+    for (i = 0; i < number; i++) {
+      let square = '';
+      for (j = 0; j < number; j++) {
+        square += 'x';
+      }
+      console.log(square);
+    }
+  } else {
+    console.log('Missing size');
+  }
+}
