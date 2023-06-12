7b24a06ed84a49f4f40a38a42ba0ee8770acf112
chriss1525 <okothchris15@gmail.com>
2023-06-12 22:43:04 +0300

    concatenate strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let i = 2;
+const i = 2;
 if (args[i]) {
   console.log(args[i].concat(' is ', args[i + 1]));
-}
-else{
+} else {
   console.log('undefined is undefined');
 }
