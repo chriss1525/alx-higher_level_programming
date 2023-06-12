3e3154aebd98b980e31a282a3f509afd3a036e43
chriss1525 <okothchris15@gmail.com>
2023-06-13 00:20:40 +0300

    print string x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const number = parseInt(args[2], 10);
+let i;
+
+if (args[2]) {
+  if (number) {
+    for (i = 0; i < number; i++) {
+      console.log('C is fun');
+    }
+  } else {
+    console.log('Missing number of occurrences');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
