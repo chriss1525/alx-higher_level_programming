c3e535e5dcef647e2b8180490ce186188fce7ea1
chriss1525 <okothchris15@gmail.com>
2023-06-12 21:44:00 +0300

    print first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (args.length <= 2) {
-  console.log('No argument');
+if (args[2]) {
+  console.log(args[2]);
 } else {
-  console.log(process.argv[2]);
+  console.log('No argument');
 }
