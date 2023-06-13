2624b3af1237b85970f7db4593e527875f5d34c7
chriss1525 <okothchris15@gmail.com>
2023-06-13 16:44:28 +0300

    add printchar(c) method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
