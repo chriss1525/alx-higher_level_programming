fc64668481c08737edcd8b372e7d9faeb7e89526
chriss1525 <okothchris15@gmail.com>
2023-06-13 14:00:50 +0300

    create double and rotate methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let iter = 0; iter < this.height; iter++) {
+      let row = '';
+      for (let iter2 = 0; iter2 < this.width; iter2++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
