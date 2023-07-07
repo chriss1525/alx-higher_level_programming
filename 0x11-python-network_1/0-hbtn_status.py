727fd16f6d9aa3c5b36c166827f76021efb04927
chriss1525 <okothchris15@gmail.com>
2023-07-07 13:08:09 +0300

    fetch status code using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-"""Fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    """Fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
         body_response = response.read()
 
 print('Body response:\n\t- type: {}'.format(type(body_response)))
