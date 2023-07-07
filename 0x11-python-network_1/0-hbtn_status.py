4916756dab21251e1f77996e913506f2115cecee
chriss1525 <okothchris15@gmail.com>
2023-07-07 13:02:13 +0300

    fetch status code using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,10 @@
 
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__": 
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body_response = response.read()
+
+print('Body response:\n\t- type: {}'.format(type(body_response)))
+print('\t- content: {}'.format(body_response))
+print('\t- utf8 content: {}'.format(body_response.decode('utf-8')))
