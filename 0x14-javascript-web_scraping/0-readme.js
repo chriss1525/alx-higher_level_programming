bdb1ecea297d36adf6d31bf3e1e242b69beb203d
chriss1525 <okothchris15@gmail.com>
2023-07-25 15:43:54 +0300

    read the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+var file = process.argv[2] || __filename;
+
+fs.readFile(file, 'utf8', function(err, data) {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+  console.log(data);
+});
