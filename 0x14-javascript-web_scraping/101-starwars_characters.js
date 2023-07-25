017d092169a8aa937228a1236dd6fd78e0dfebf8
chriss1525 <okothchris15@gmail.com>
2023-07-25 17:42:41 +0300

    list all characters in a star wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,19 +4,31 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 const movieId = process.argv[2];
 const log = console.log;
 
+function printCharactersInOrder(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  const character = characters[index];
+  request(character, function (error, response, body) {
+    if (error) {
+      log(error);
+    } else {
+      const name = JSON.parse(body).name;
+      log(name);
+    }
+
+    // Recursively call the function to print the next character in order
+    printCharactersInOrder(characters, index + 1);
+  });
+}
+
 request(url + movieId, function (error, response, body) {
   if (error) {
     log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    characters.forEach(character => {
-      request(character, function (error, response, body) {
-        if (error) {
-          log(error);
-        } else {
-          log(JSON.parse(body).name);
-        }
-      });
-    });
+    printCharactersInOrder(characters, 0);
   }
 });
+
