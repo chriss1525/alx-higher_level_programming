53a220f493fb8d86a13e802a4493ea6ef8b6ee91
chriss1525 <okothchris15@gmail.com>
2023-08-02 16:49:09 +0300

    use jquery to change header colour

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,1 @@
-#!/usr/bin/node
-
-document.addeventListener('DOMContentLoaded', function () {
-  document.querySelector('header').style.color = '#FF0000';
-}
+document.querySelector('header').style.color = '#FF0000';
