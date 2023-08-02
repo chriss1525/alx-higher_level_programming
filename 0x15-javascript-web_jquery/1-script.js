f9f1e28873a2f8a44f48e337205f7f4c7cb19168
chriss1525 <okothchris15@gmail.com>
2023-08-02 17:26:36 +0300

    toggle through header colors

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,1 +1,3 @@
-$('header').css('color', '#FF0000');
+$(document).ready(function(){
+  $("header").css("color", "#FF0000");
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#red_header").click(function(){
+    $("header").css("color", "#FF0000");
+  });
+});
+
